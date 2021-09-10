55cc38bd9094ad874d380aac11514d188cbcd5da
Bobby Smith <reversetype@gmail.com>
2021-09-10 10:17:00 -0400

    update to staticquery

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,54 +1,56 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import { useStaticQuery, graphql } from 'gatsby';
+import { graphql, StaticQuery } from 'gatsby';
 
-const SEO = ({ title, description, image }) => {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            image
-            siteUrl
+export default function SEO({ description, image, title }) {
+  return (
+    <StaticQuery
+      query={graphql`
+        query HeadingQuery {
+          site {
+            siteMetadata {
+              title
+              description
+              image
+              siteUrl
+            }
           }
         }
-      }
-    `
-  );
+      `}
+      render={(data) => {
+        const defaultImage = data.site.siteMetadata.siteUrl + data.site.siteMetadata.image;
+        const metaDescription = description || data.site.siteMetadata.description;
+        const metaImage = image || defaultImage;
+        // console.log('🚀 data: ', data);
 
-  const defaultImage = site.siteMetadata.siteUrl + site.siteMetadata.image;
-  const metaDescription = description || site.siteMetadata.description;
-  const metaImage = image || defaultImage;
+        return (
+          <Helmet
+            htmlAttributes={{
+              lang: `en`,
+            }}
+            title={title}
+            defaultTitle={data.site.siteMetadata.title}
+            titleTemplate={`%s | ${data.site.siteMetadata.title}`}
+          >
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            {/* General tags */}
+            <meta name="image" content={image} />
+            <meta name="description" content={metaDescription} />
 
-  return (
-    <Helmet
-      htmlAttributes={{
-        lang: `en`,
+            {/* OpenGraph tags */}
+            <meta property="og:title" content={title} />
+            <meta property="og:image" content={metaImage} />
+            <meta property="og:description" content={metaDescription} />
+
+            {/* Twitter Card tags */}
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:title" content={title} />
+            <meta name="twitter:image" content={metaImage} />
+            <meta name="twitter:description" content={metaDescription} />
+          </Helmet>
+        );
       }}
-      title={title}
-      defaultTitle={site.siteMetadata.title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
-    >
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      {/* General tags */}
-      <meta name="image" content={image} />
-      <meta name="description" content={metaDescription} />
-
-      {/* OpenGraph tags */}
-      <meta property="og:title" content={title} />
-      <meta property="og:image" content={metaImage} />
-      <meta property="og:description" content={metaDescription} />
-
-      {/* Twitter Card tags */}
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:image" content={metaImage} />
-      <meta name="twitter:description" content={metaDescription} />
-    </Helmet>
+    />
   );
-};
-
-export default SEO;
+}
